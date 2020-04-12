1a39fc826ac7524e5f8f163c6fcad57460d95461
phaneendrakumarcv <48360841+phaneendrakumarcv@users.noreply.github.com>
2020-04-13 04:33:20 +0530

    Modified 3 convolution layers
    Added extra convolution layers

--- coviddetector.py
+++ coviddetector.py
@@ -25,13 +25,13 @@ def DefineModel():
         model.add(Conv2D(512,kernel_size=(3,3),activation='relu',padding='same'))
         model.add(MaxPooling2D(pool_size=(2,2)))
 
-        model.add(Conv2D(512,kernel_size=(3,3),padding='same'))
+        model.add(Conv2D(512,kernel_size=(3,3),activation='relu',padding='same'))
         model.add(MaxPooling2D(pool_size=(2,2)))
 
-        model.add(Conv2D(256,kernel_size=(3,3),padding='same'))
+        model.add(Conv2D(256,kernel_size=(3,3),activation='relu',padding='same'))
         model.add(MaxPooling2D(pool_size=(2,2)))
 
-        model.add(Conv2D(256,kernel_size=(3,3),padding='same'))
+        model.add(Conv2D(256,kernel_size=(3,3),activation='relu',padding='same'))
         model.add(MaxPooling2D(pool_size=(2,2)))
 
         model.add(Flatten())
